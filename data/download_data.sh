70e6bba720778486cafa2f50490ebb573260773a
Mohit Shridhar <mohit.appi@gmail.com>
2020-10-14 19:33:53 -0700

    readme
    Former-commit-id: 24fbad32c9a27be2f20892c1c987415ad6e0c904 [formerly df91a90e6d69bdb4a4e5755a298c9fd85629e127] [formerly 24fbad32c9a27be2f20892c1c987415ad6e0c904 [formerly df91a90e6d69bdb4a4e5755a298c9fd85629e127] [formerly df91a90e6d69bdb4a4e5755a298c9fd85629e127 [formerly 3763735a2bd67d1ad72e4ec8fd9a5a70f06b6b1d]]]
    Former-commit-id: 5aa1d6f47701e5bae06f8e545dcd1d44b362a0a5
    Former-commit-id: 649f2889faf6e3d6fb3074392c138100eb0621d0 [formerly d990580da1f7ceb7bd371a7b926dd22f7f237a8b]
    Former-commit-id: 2d59d839470e803534af8e7cd2055328331f8f98

--- data/download_data.sh
+++ data/download_data.sh
@@ -1,41 +1,17 @@
 #!/bin/bash
 
-# Install 7z
-echo "Checking for 7z (might require installation)..."
-
-if sudo apt-get install p7zip-full -y; then
-    echo "7z found/installed"
-else
-    echo "Failed: Please install 7z (https://www.7-zip.org/7z.html)"
-    exit 1
-fi
-
 # Download, Unzip, and Remove zip
-if [ "$1" = "json" ]
-then
 
-    echo "Downloading JSONs ..."
-    wget https://ai2-vision-alfred.s3-us-west-2.amazonaws.com/json_2.1.0.7z
-    7z x json_2.1.0.7z -y && rm json_2.1.0.7z
-    echo "saved folder: json_2.1.0"
+cd $ALFRED_ROOT/data
 
-elif [ "$1" = "json_feat" ]
-then
+wget https://bit.ly/3cIEx9R
+mv 3cIEx9R 3cIEx9R.zip
+unzip 3cIEx9R.zip & rm 3cIEx9R.zip
 
-    echo "Downloading JSONs and Resnet18 feats ..."
-    wget https://ai2-vision-alfred.s3-us-west-2.amazonaws.com/json_feat_2.1.0.7z
-    7z x json_feat_2.1.0.7z -y && rm json_feat_2.1.0.7z
-    echo "saved folder: json_feat_2.1.0"
+wget https://bit.ly/3mZsrhf
+mv 3mZsrhf 3mZsrhf.zip
+unzip 3mZsrhf.zip & rm 3mZsrhf.zip
 
-elif [ "$1" = "full" ]
-then
-
-    echo "Downloading JSONs, Resnet18 feats, Images, Videos, and PDDL states ..."
-    wget https://ai2-vision-alfred.s3-us-west-2.amazonaws.com/full_2.1.0.7z
-    7z x full_2.1.0.7z -y && rm full_2.1.0.7z
-    echo "saved folder: full_2.1.0"
-
-else
-    echo "Failed: Usage download_data.sh json | json_feat | full"
-    exit 1
-fi
+wget https://bit.ly/2S8lexl
+mv 2S8lexl 2S8lexl.zip
+unzip 2S8lexl.zip & rm 2S8lexl.zip
