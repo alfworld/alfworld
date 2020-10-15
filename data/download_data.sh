338ff1fe4e802ad28997b619df46637f3db98d03
Mohit Shridhar <mohit.appi@gmail.com>
2020-10-14 21:57:17 -0700

    minor readme
    Former-commit-id: 1cbabcf3b3e430dd4ceb460a8b2576a662dfdb6b

--- data/download_data.sh
+++ data/download_data.sh
@@ -6,12 +6,15 @@ cd $ALFRED_ROOT/data
 
 wget https://bit.ly/3cIEx9R
 mv 3cIEx9R 3cIEx9R.zip
-unzip 3cIEx9R.zip & rm 3cIEx9R.zip
+unzip 3cIEx9R.zip
+rm 3cIEx9R.zip
 
 wget https://bit.ly/3mZsrhf
 mv 3mZsrhf 3mZsrhf.zip
-unzip 3mZsrhf.zip & rm 3mZsrhf.zip
+unzip 3mZsrhf.zip
+rm 3mZsrhf.zip
 
 wget https://bit.ly/2S8lexl
 mv 2S8lexl 2S8lexl.zip
-unzip 2S8lexl.zip & rm 2S8lexl.zip
+unzip 2S8lexl.zip
+rm 2S8lexl.zip
